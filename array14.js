bc1b553b41698d3c7f47bd05c92c1cb9f94282dc
kheyam khan <khankheyam41@gmail.com>
2026-06-16 08:37:16 +0100

    02_basics

---
+++ array14.js
@@ -0,0 +1,34 @@
+//Array
+let array=[1,2,3,4,5]
+let array2=["kheyam",'khan',"good"]
+let array3=new Array(1,2,3,4,5,6)
+// console.log(array3[2]);
+
+//Array meythod
+// array.push(12)
+// array.push(14)
+// array.pop()
+// array.unshift(12)
+// array.shift()
+// console.log(array.includes(4));
+// console.log(array.indexOf(4));
+// const newarray=array.join()
+// console.log(array);
+// console.log(newarray);
+
+
+//slice and splice 
+console.log("A",array);
+const myn1=array.slice(1,3)
+console.log("B",array);
+console.log(myn1);
+const myn2=array.splice(1,3)
+console.log("C",array);
+console.log(myn2);
+
+
+
+
+
+
+
