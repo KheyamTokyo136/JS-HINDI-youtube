24925c66c98ca25f91297d4155ae8a7ff3c65afe
kheyam khan <khankheyam41@gmail.com>
2026-07-10 18:50:55 +0100

    Add files via upload

---
+++ 10_Classes_and_Objects/object_set_get.js
@@ -0,0 +1,16 @@
+const user={
+    _email:"khankheyam@gmail.com",
+    _password:"abc",
+    get email()
+    {
+        return this._email.toUpperCase()
+    },
+    set email(value)
+    {
+        this._email=value
+    }
+
+}
+
+const chai= Object.create(user)
+console.log(chai.email);
